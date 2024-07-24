18219ef1acea30c7fb1aa77d4e657a35956e6cda
Shobana-31 <shobananarasimman31@gmail.com>
2024-07-24 14:30:55 +0530

    13 Upto 15

--- Quesion_Paper2/Program.cs
+++ Quesion_Paper2/Program.cs
@@ -16,7 +16,12 @@ namespace Quesion_Paper2
            // q4();
             //q5();
             //q6();
-            q7();
+           // q7();
+           // q8();
+           // q9();
+           // q10();
+            //q11();
+            q12();
         }
         public static void q1()
         {
@@ -148,8 +153,120 @@ namespace Quesion_Paper2
             {
                 Console.WriteLine("Invaild Data");
             }
-
         }
-
+        public static void q8()
+        {
+            Console.WriteLine("To Find Maximum Value");
+            Console.WriteLine("Enter Number1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int num3 = int.Parse(Console.ReadLine());
+            int max = num1;
+            if (num2>max)
+            {
+                max = num2; 
+            }
+            else if (num3>max)
+            {
+                max = num3;
+            }
+            Console.WriteLine("Maximum Number is:"+max);
+        }
+        public static void q9()
+        {
+            Console.WriteLine("To Find Minimum Value");
+            Console.WriteLine("Enter Number1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int num3 = int.Parse(Console.ReadLine());
+            int min = num1;
+            if (num2 < min)
+            {
+                min = num2;
+            }
+            else if (num3 < min)
+            {
+                min = num3;
+            }
+            Console.WriteLine("Minimum Value:"+min);
+        }
+        public static void q10()
+        {
+            Console.WriteLine("To Find Maximum Value");
+            Console.WriteLine("Enter Number1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number4");
+            int num4 = int.Parse(Console.ReadLine());
+            int max = num1;
+            if (num2 > max)
+            {
+                max = num2;
+            }
+            else if (num3 > max)
+            {
+                max = num3;
+            }
+            else if (num4 > max)
+            {
+                max = num4;
+            }
+            Console.WriteLine("Maximum Number is:" + max);
+        }
+        public static void q11()
+        {
+            Console.WriteLine("Enter Number1");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int b= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int c = int.Parse(Console.ReadLine());
+            int ab_diff =Math.Abs(a - b);
+            int bc_diff =Math .Abs(b - c);
+            int ac_diff =Math .Abs (a - c);
+            if (ab_diff<bc_diff && ab_diff <ac_diff )
+            {
+                Console.WriteLine("A and B Closer");
+            }
+            if (bc_diff <ab_diff && bc_diff <ac_diff )
+            {
+                Console.WriteLine("B and C Closer");
+            }
+            if (ac_diff < ab_diff && ac_diff < bc_diff)
+            {
+                Console.WriteLine("C and A Closer");
+            }           
+        }
+        public static void q12()
+        {
+            Console.WriteLine("Enter Number1");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number2");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Number3");
+            int c = int.Parse(Console.ReadLine());
+            int ab_diff = Math.Abs(a - b);
+            int bc_diff = Math.Abs(b - c);
+            int ac_diff = Math.Abs(a - c);
+            if (ab_diff > bc_diff && ab_diff > ac_diff)
+            {
+                Console.WriteLine("A and B Farther");
+            }
+            if (bc_diff > ab_diff && bc_diff > ac_diff)
+            {
+                Console.WriteLine("B and C Farther");
+            }
+            if (ac_diff > ab_diff && ac_diff > bc_diff)
+            {
+                Console.WriteLine("C and A Farther");
+            }
+        }
     }
 }
